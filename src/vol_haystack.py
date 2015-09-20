4d04866fe8e4d787df157bf790d30da7cefdcb02
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2015-09-20 00:18:13 -0600

    refactor to handle reverse

--- src/vol_haystack.py
+++ src/vol_haystack.py
@@ -24,28 +24,8 @@ class Haystack(taskmods.DllList):
     """
     Search for a record in all the memory space.
     """
-    def __init__(self, config, *args, **kwargs):
-        self.config = config
-        taskmods.DllList.__init__(self, config, *args, **kwargs)
-        config.add_option('RECORD_NAME', short_option='r', default= None,
-                          help='Search for this record type',
-                          action='store', type='str')
-        config.add_option('CONSTRAINT_FILE', short_option='c', default= None,
-                          help='Using this constraint file',
-                          action='store', type='str')
 
-    def _init_haystack(self):
-        # get the structure name and type
-        self.modulename, sep, self.classname = self.config.RECORD_NAME.rpartition('.')
-        # parse the constraint file
-        if self.config.CONSTRAINT_FILE:
-            handler = constraints.ConstraintsConfigHandler()
-            self.my_constraints = handler.read(self.config.CONSTRAINT_FILE)
-        else:
-            self.my_constraints = None
-        return
-
-    def _search(self, task):
+    def _do_haystack(self, task):
         pid = task.UniqueProcessId
         my_mappings = []
         # get the mappings
@@ -94,15 +74,48 @@ class Haystack(taskmods.DllList):
         # create a memory handler
         memory_handler = base.MemoryHandler(my_mappings, my_target, self.config.LOCATION)
 
+        for res in self.make_results(pid, memory_handler):
+            yield res
+
+    def make_results(self, pid, memory_handler):
+        raise NotImplementedError('Implement me')
+
+
+class HaystackSearch(Haystack):
+    """
+    Search for a record in all the memory space.
+    """
+    def __init__(self, config, *args, **kwargs):
+        self.config = config
+        taskmods.DllList.__init__(self, config, *args, **kwargs)
+        config.add_option('RECORD_NAME', short_option='r', default= None,
+                          help='Search for this record type',
+                          action='store', type='str')
+        config.add_option('CONSTRAINT_FILE', short_option='c', default= None,
+                          help='Using this constraint file',
+                          action='store', type='str')
+
+    def _init_haystack(self):
+        # get the structure name and type
+        self.modulename, sep, self.classname = self.config.RECORD_NAME.rpartition('.')
+        # parse the constraint file
+        if self.config.CONSTRAINT_FILE:
+            handler = constraints.ConstraintsConfigHandler()
+            self.my_constraints = handler.read(self.config.CONSTRAINT_FILE)
+        else:
+            self.my_constraints = None
+        return
+
+    def make_results(self, pid, memory_handler):
         # import the record class in the haystack model
         # we need pwd in path
         sys.path.append(os.getcwd())
         module = memory_handler.get_model().import_module(self.modulename)
         struct_type = getattr(module, self.classname)
-        for res in self.make_results(memory_handler, struct_type, self.my_constraints):
+        for res in self.make_search_results(memory_handler, struct_type, self.my_constraints):
             yield pid, res
 
-    def make_results(self, memory_handler, struct_type, my_constraints):
+    def make_search_results(self, memory_handler, struct_type, my_constraints):
         # do the search
         # do not use the haystack HEAP parsing capabilities
         ## PROD - use API
@@ -122,7 +135,7 @@ class Haystack(taskmods.DllList):
         tasks = taskmods.DllList.calculate(self)
         results = []
         for task in tasks:
-            results.extend([(pid, addr) for pid, addr in self._search(task)])
+            results.extend([(pid, addr) for pid, addr in self._do_haystack(task)])
         return results
 
     def render_text(self, outfd, data):
@@ -140,11 +153,11 @@ class Haystack(taskmods.DllList):
 #                       ], self.generator(data))
 
 
-class HaystackHeap(Haystack):
+class HaystackHeap(HaystackSearch):
     """
     Search for a record in an optimised way, suitable for windows HEAP search.
     """
-    def make_results(self, memory_handler, struct_type, my_constraints):
+    def make_search_results(self, memory_handler, struct_type, my_constraints):
         ## DEBUG - use optimised search space for HEAP
         my_searcher = searcher.AnyOffsetRecordSearcher(memory_handler, my_constraints)
         for mapping in memory_handler.get_mappings():
@@ -155,11 +168,11 @@ class HaystackHeap(Haystack):
             ## use direct load
             # results = api.load_record(memory_handler, struct_type, 0x150000, load_constraints=None)
 
-class HaystackAllocated(Haystack):
+class HaystackAllocated(HaystackSearch):
     """
     Search for a record only in allocated memory chunks.
     """
-    def make_results(self, memory_handler, struct_type, my_constraints):
+    def make_search_results(self, memory_handler, struct_type, my_constraints):
         # do the search
         # USE the haystack HEAP parsing capabilities
         ## PROD - use API
@@ -169,18 +182,17 @@ class HaystackAllocated(Haystack):
         for instance, addr in ret:
             yield addr
 
-class HaystackShow(Haystack):
+class HaystackShow(HaystackSearch):
     """
     Show the record value
     """
     def __init__(self, config, *args, **kwargs):
-        Haystack.__init__(self, config, *args, **kwargs)
+        HaystackSearch.__init__(self, config, *args, **kwargs)
         config.add_option('ADDRESS', short_option='a', default= None,
                           help='Using this address (hex) to load the record',
                           action='store', type='str')
 
-    def make_results(self, memory_handler, struct_type, my_constraints):
-        #if self.config.ADDRESS:
+    def make_search_results(self, memory_handler, struct_type, my_constraints):
         addr = int(self.config.ADDRESS, 16)
         results = api.load_record(memory_handler, struct_type, addr, load_constraints=my_constraints)
         instance = api.output_to_string(memory_handler, [results])
@@ -194,3 +206,65 @@ class HaystackShow(Haystack):
             outfd.write('Record content:\n')
             outfd.write(instance)
 
+
+
+class HaystackReverse(Haystack):
+    """
+    Reverse all the allocated records of a process memory.
+    """
+    def __init__(self, config, *args, **kwargs):
+        self.config = config
+        taskmods.DllList.__init__(self, config, *args, **kwargs)
+
+    def make_results(self, pid, memory_handler):
+        from haystack.reverse import context
+        log.debug('[+] Loading the memory dump ')
+        ctx = context.get_context(dumpname)
+        try:
+            if not os.access(config.get_record_cache_folder_name(ctx.dumpname), os.F_OK):
+                os.mkdir(config.get_record_cache_folder_name(ctx.dumpname))
+
+            log.info("[+] Cache created in %s", config.get_record_cache_folder_name(ctx.dumpname))
+
+            # we use common allocators to find structures.
+            #log.debug('Reversing malloc')
+            #mallocRev = MallocReverser()
+            #ctx = mallocRev.reverse(ctx)
+            # mallocRev.check_inuse(ctx)
+
+            # try to find some logical constructs.
+            log.debug('Reversing DoubleLinkedListReverser')
+            doublelink = DoubleLinkedListReverser(ctx)
+            ctx = doublelink.reverse(ctx)
+
+            # decode bytes contents to find basic types.
+            log.debug('Reversing Fields')
+            fr = FieldReverser(ctx)
+            ctx = fr.reverse(ctx)
+
+            # identify pointer relation between structures
+            log.debug('Reversing PointerFields')
+            pfr = PointerFieldReverser(ctx)
+            ctx = pfr.reverse(ctx)
+
+            # graph pointer relations between structures
+            log.debug('Reversing PointerGraph')
+            ptrgraph = PointerGraphReverser(ctx)
+            ctx = ptrgraph.reverse(ctx)
+            ptrgraph._saveStructures(ctx)
+
+            # save to file
+            save_headers(ctx)
+            # fr._saveStructures(ctx)
+            ##libRev = KnowStructReverser('libQt')
+            ##ctx = libRev.reverse(ctx)
+            # we have more enriched context
+
+            # etc
+        except KeyboardInterrupt as e:
+            # except IOError,e:
+            log.warning(e)
+            log.info('[+] %d structs extracted' % (ctx.structuresCount()))
+            raise e
+            pass
+        pass
